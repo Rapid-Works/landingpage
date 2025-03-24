463b20cea36c5e8888ff295ce06a3c247172fbea
Samuel Donkor <calvinsamueldonkor@gmail.com>
2025-03-24 10:11:41 +0000

    formula v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
           xl: '1280px',
         },
       },
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif'],
+      },
     }
   },
   plugins: [
